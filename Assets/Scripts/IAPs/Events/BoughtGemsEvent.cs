2f908a09484b846ed7293a42f90d3fbfbe24c056
Fuan <fuanfrost@gmail.com>
2026-04-09 23:43:07 -0400

    settings

--- Assets/Scripts/IAPs/Events/BoughtGemsEvent.cs
+++
@@ -1,8 +0,0 @@
-using Unity.Services.Analytics;
-
-public class BoughtGemsEvent : Event
-{
-    public BoughtGemsEvent() : base("boughtGems") { }
-
-    public int GemAmountPurchased { set { SetParameter("gemAmountPurchased", value); } }
-}
--- Assets/Scripts/IAPs/Events/GemAdViewedEvent.cs
+++
@@ -1,8 +0,0 @@
-using Unity.Services.Analytics;
-
-public class GemAdViewedEvent : Event
-{
-    public GemAdViewedEvent() : base("gemAdViewed") { }
-
-    public string GemAdTypeWatched { set { SetParameter("gemAdTypeWatched", value); } }
-}
