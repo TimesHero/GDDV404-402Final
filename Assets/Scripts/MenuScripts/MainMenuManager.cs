b1738376667c89759a09c1a83274fa61f0046ea0
AJ-FW16-CachyOS <alexander.aj.fraser@gmail.com>
2026-04-17 17:24:53 -0400

    Main Menu, Settings Menu Foundation
    On the same scene as the store, I made the camera pan from one location to another depending on the button pressed.

--- Assets/Scripts/MenuScripts/MainMenuManager.cs
+++ Assets/Scripts/MenuScripts/MainMenuManager.cs
@@ -1,16 +1,10 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void LoadGameScene(string sceneName)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(sceneName);
     }
 }
